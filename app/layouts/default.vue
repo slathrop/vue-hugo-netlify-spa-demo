cacadc9219efa736ce5bbf4136cb427e4a1c49ac
SLathrop <steve@stela.io>
2018-05-20 18:47:08 -0400

    WIP app: cleanup

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -17,17 +17,7 @@
       <v-btn icon @click.stop="miniVariant = !miniVariant">
         <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'"></v-icon>
       </v-btn>
-      <!-- <v-btn icon @click.stop="clippedLeftDrawer = !clippedLeftDrawer">
-        <v-icon>web</v-icon>
-      </v-btn> -->
-      <!-- <v-btn icon @click.stop="fixedFooter = !fixedFooter">
-        <v-icon>remove</v-icon>
-      </v-btn> -->
       <v-toolbar-title v-text="title"></v-toolbar-title>
-      <!-- <v-spacer></v-spacer>
-      <v-btn icon @click.stop="">
-        <v-icon>menu</v-icon>
-      </v-btn> -->
     </v-toolbar>
     <v-content>
       <v-container>
