4fa3373d60b0205f98e4f913a23a33c2c40cea67
SLathrop <steve@stela.io>
2018-05-20 18:22:55 -0400

    WIP app: rm right drawer and menu opt

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -24,26 +24,16 @@
         <v-icon>remove</v-icon>
       </v-btn>
       <v-toolbar-title v-text="title"></v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
+      <!-- <v-spacer></v-spacer>
+      <v-btn icon @click.stop="">
         <v-icon>menu</v-icon>
-      </v-btn>
+      </v-btn> -->
     </v-toolbar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer temporary :right="right" v-model="rightDrawer" fixed>
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer :fixed="fixed" app>
       <span>&copy; 2017</span>
     </v-footer>
@@ -62,8 +52,6 @@ export default {
         { icon: "bubble_chart", title: "Inspire", to: "/inspire" }
       ],
       miniVariant: false,
-      right: true,
-      rightDrawer: false,
       title: "Vuetify.js"
     };
   }
