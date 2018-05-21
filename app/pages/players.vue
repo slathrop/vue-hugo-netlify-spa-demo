8c4ba0f3a09884659a5f458df5887356e84dd5e1
SLathrop <steve@stela.io>
2018-05-21 01:31:25 -0400

    WIP app: players

--- app/pages/players.vue
+++ app/pages/players.vue
@@ -1,7 +1,7 @@
 <template>
   <v-layout>
     <v-flex>
-      <v-data-table :must-sort="true" :headers="headers" :items="players" hide-actions>
+      <v-data-table :must-sort="true" :headers="headers" :items="rows" hide-actions>
         <template slot="items" slot-scope="props">
           <td>{{ props.item.RowLabel }}</td>
           <td>{{ props.item.LName }}</td>
@@ -19,22 +19,23 @@ const API_HOST = 'https://bobble-api.netlify.com';
 // const API_HOST = 'http://localhost:1313';
 const API_ENTITY_BASE_URL = '/players';
 const API_GET_SUFFIX = '/get.json';
+const API_RESULT_DATA_ROWS_KEY = 'playerBobbles';
 
 export default {
   data: () => ({
     loading: true,
     pagination: {
-      sortBy: 'LName'
+      sortBy: 'LastName'
     },
     mustSort: true,
     headers: [
       { text: '', value: 'RowLabel', sortable: false, align: 'left' },
-      { text: 'Last Name', value: 'LName', align: 'left' },
-      { text: 'First Name', value: 'FName', align: 'left' },
+      { text: 'Last Name', value: 'LastName', align: 'left' },
+      { text: 'First Name', value: 'FirstName', align: 'left' },
       { text: 'Team', value: 'Team', align: 'left' },
       { text: 'Bobble Pose', value: 'Pose', align: 'left' }
     ],
-    players: [],
+    rows: [],
 
     dataUrl: `${API_HOST}${API_ENTITY_BASE_URL}/all${API_GET_SUFFIX}`
   }),
@@ -53,12 +54,12 @@ export default {
       this.loading = true;
 
       try {
-        console.log('Getting players...');
+        console.log('Getting rows...');
         const response = await this.$axios.$get(this.dataUrl);
-        console.log('Got players');
-        this.players = response.data.playerBobbles;
+        console.log('Got rows');
+        this.rows = response.data[API_RESULT_DATA_ROWS_KEY];
       } catch (e) {
-        console.log('Error getting players:', e);
+        console.log('Error getting rows:', e);
       }
 
       this.loading = false;
